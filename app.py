5e796aa7e78bb324c08be920c2cffe8959be8e48
Israel Avihail <bilbisli@gmail.com>
2023-04-01 17:01:55 +0300

    added newsflash data

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, render_template, request, url_for
+import pymongo
 from pymongo import MongoClient
 import requests
 
@@ -9,6 +10,7 @@ app = Flask(__name__)
 client = MongoClient("mongodb+srv://bilbisli:GoKAMYlEnwV5wC6G@cluster0.jddwckk.mongodb.net/?retryWrites=true&w=majority")
 db = client['app_db']
 collection = db['news_flash']
+# db.news_flash.create_index([('Title', pymongo.TEXT), ('Text', pymongo.TEXT)], name='newsflash_index', default_language='none', unique=True)
 
 # define the scraping tools and websites as lists
 scraping_tools = ['requests', 'BeautifulSoup', 'selenium']
@@ -21,12 +23,14 @@ def add_to_db(df):
 @app.route('/')
 def newsflash_page():
     news_flash = list(db.news_flash.find())
-    fields = list(news_flash[1].keys())[1:]
-    return render_template('newsflash_page.html', websites=websites.keys(), news_flash=news_flash, fields=fields)
+    fields = list(news_flash[1].keys())[1:] if news_flash else []
+    date_fields = list(filter(lambda f: 'date'.casefold() in f.casefold(), fields))
+    sort_by_field = date_fields[0] if date_fields else None
+    return render_template('newsflash_page.html', websites=websites.keys(), news_flash=news_flash, fields=fields, sort_by_field=sort_by_field)
 
 @app.route('/scrape', methods=['POST'])
 def scrape():
-    # get the selected scraping tool and website from the form
+    # get the selected scraping website from the form
     website = request.form.get('website')
     websites[website](add_to_db)
     
